fc3a4240732259fc090837e26981a080752c2af3
Natanael Lima <taelima1997@gmail.com>
2021-12-25 23:58:56 -0300

    update with get product collection realtime

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,18 +1,37 @@
-import React, { useState } from 'react';
-import { FlatList } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Alert, FlatList } from "react-native";
+import firestore from "@react-native-firebase/firestore";
 
-import { styles } from './styles';
-import { Product, ProductProps } from '../Product';
+import { styles } from "./styles";
+import { Product, ProductProps } from "../Product";
 
-import { shoppingListExample } from '../../utils/shopping.list.data';
 
 export function ShoppingList() {
-  const [products, setProducts] = useState<ProductProps[]>(shoppingListExample);
+  const [products, setProducts] = useState<ProductProps[]>([]);
+
+  useEffect(() => {
+    const subscriber = firestore()
+      .collection("products")
+      .onSnapshot((documentSnapshot) => {
+        setProducts(
+          documentSnapshot.docs.map((d) => ({
+            id: d.id,
+            description: d.data().description,
+            quantity: d.data().quantity,
+            done: d.data().done,
+          }))
+        );
+      }, (error) => {
+          Alert.alert(error.message)
+      });
+
+    return () => subscriber();
+  }, []);
 
   return (
     <FlatList
-      data={shoppingListExample}
-      keyExtractor={item => item.id}
+      data={products}
+      keyExtractor={(item) => item.id}
       renderItem={({ item }) => <Product data={item} />}
       showsVerticalScrollIndicator={false}
       style={styles.list}
