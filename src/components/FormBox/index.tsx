10e4550dbdef965f5127b334ca802c656c92a6d0
Natanael Lima <taelima1997@gmail.com>
2021-12-25 23:36:39 -0300

    add handleCreateProduct with firestore

--- src/components/FormBox/index.tsx
+++ src/components/FormBox/index.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
+import firestore from "@react-native-firebase/firestore";
 
-import { Container } from './styles';
-import { ButtonIcon } from '../ButtonIcon';
-import { Input } from '../Input';
+import { Container } from "./styles";
+import { ButtonIcon } from "../ButtonIcon";
+import { Input } from "../Input";
+import { Alert } from "react-native";
 
 export function FormBox() {
+  const [description, setDescription] = useState("");
+  const [quantiry, setQuantiry] = useState(0);
+
+  async function handleCreateProduct() {
+    try {
+      const productRef = await firestore()
+        .collection("products")
+        .add({ description, quantiry, done: false });
+
+      const product = await productRef.get();
+
+      console.log(product.data());
+    } catch (error: any) {
+      console.log(error);
+      Alert.alert(error.message);
+    }
+  }
+
   return (
     <Container>
       <Input
+        onChangeText={setDescription}
         placeholder="Nome do produto"
         size="medium"
       />
 
       <Input
+        onChangeText={(value) => setQuantiry(Number(value))}
         placeholder="0"
         keyboardType="numeric"
         size="small"
@@ -20,9 +42,9 @@ export function FormBox() {
       />
 
       <ButtonIcon
-        size='large'
+        size="large"
         icon="add-shopping-cart"
-        onPress={() => { }}
+        onPress={handleCreateProduct}
       />
     </Container>
   );
