54d4a2d548234d1dc6b7225fa63dabccfe089ab4
Natanael Lima <taelima1997@gmail.com>
2021-12-29 00:16:14 -0300

    add subscriber user logged to redirect routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import auth from "@react-native-firebase/auth";
 
-import { AppRoutes } from './app.routes';
-import { SignIn } from '../screens/SignIn';
+import { AppRoutes } from "./app.routes";
+import { SignIn } from "../screens/SignIn";
 
 export function Routes() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  function onAuthStateChanged(user: any) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
   return (
     <NavigationContainer>
-      <AppRoutes />
+      {user ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
-  )
+  );
 }
